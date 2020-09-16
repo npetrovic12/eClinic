63c79d703af344d085a27be8304a3c20e092b2cf
npetrovic12 <nikola20petrovic@gmail.com>
2020-09-16 17:00:44 +0200

    Removed import

--- src/main/webapp/app/scheduler/scheduler.route.ts
+++ src/main/webapp/app/scheduler/scheduler.route.ts
@@ -1,6 +1,4 @@
 import { Route } from '@angular/router';
-
-import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
 import { SchedulerComponent } from './scheduler.component';
 
 export const schedulerRoute: Route = {
