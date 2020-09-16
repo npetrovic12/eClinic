72787840281516dbd6973e2bbb4518834549087d
npetrovic12 <nikola20petrovic@gmail.com>
2020-09-16 17:51:11 +0200

    removed import

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,7 +4,6 @@ import { RouterModule } from '@angular/router';
 import { EClinicSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [EClinicSharedModule, RouterModule.forChild([HOME_ROUTE])],
